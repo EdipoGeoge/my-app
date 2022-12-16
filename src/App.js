338f75f85dfe63217e0e3c313870b1e710e4b939
EdipoGeoge <edipomds@gmail.com>
2022-12-15 23:15:41 -0300

    Vai Timao!

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Geoge</h1>      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
        
       </header>
     </div>
